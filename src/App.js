888595999790578a625cf624d6b08f1a16ea3420
Matheus Balcky <matheuscabal821@gmail.com>
2022-07-06 13:25:10 -0300

    React Files added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './styles/App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
