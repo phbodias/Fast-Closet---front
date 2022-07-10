8feb3bcde5aa5c864147a14d9999717c33a56985
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-07-10 01:32:06 -0300

    Error

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
 
       <Routes>
-        
+
         <Route path='/' element={<HomePage />} />
         <Route path='/register' element={<RegisterPage/>} />
         <Route path='/login' element={<LoginPage/>} />
